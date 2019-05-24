a57d1ca71f462b56f8baa49e437029428c46e472
Tribunals Ontario <47038550+TOCOMMUNICATIONS@users.noreply.github.com>
2019-05-24 11:32:14 -0400

    Update myScript.js

--- views/javascript/myScript.js
+++ views/javascript/myScript.js
@@ -16,21 +16,7 @@ function resizeText(multiplier) {
   }
   document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.05) + "em";
 }
- function addRow()
-{
-   var arrTables = document.getElementById('myTable');
-   var oRows = arrTables.rows;
-   var numRows = oRows.length;
-
-   var newRow = document.getElementById('myTable').insertRow( numRows );
-   var cell1 = newRow.insertCell(0);
-   var cell2 = newRow.insertCell(1);
-   var cell3 = newRow.insertCell(2);
-
-   cell1.innerHTML = 1;
-   cell2.innerHTML = 1;
-   cell3.innerHTML = 9;
-}
+ 
 function myFunction() {
   var btn = document.getElementById("myContainer");
   document.body.appendChild(btn);
